b4071bfdc1c9b07ecd5aad1687290b41b69f3c7e
ShiTatmiyu <tatmiyu01@gmail.com>
2023-09-05 18:19:45 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  "base": "/shitatmiyu.github.io",
+  "base": "./shitatmiyu.github.io",
   plugins: [react()],
 })
